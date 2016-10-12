c82c5f6fe20e1fe89560d6cb96dd7e208d25d325
WangTongLiang <1247615209@qq.com>
2016-10-12 11:53:10 +0800

    readme文件更改

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
  
 connection.connect();
 data.forEach(function(item){
-  var oneSql = `INSERT INTO users(user_id,user_name,user_age,user_gend) VALUES(${item.id},'${item.name}',${item.age},${item.gend})`;
+  var oneSql = `INSERT INTO users(user_id,user_name,user_age,user_gend) VALUES(${item.id},'${item.name}',${item.age},'${item.gend}')`;
 
 connection.query(oneSql, function(err, rows, fields) {
   if (err) throw err;
